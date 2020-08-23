f19416c63bb5651d657b3fe3d465058dc0e09b4a
azharulhaque <azharul.haque@tieto.com>
2020-08-23 16:12:21 +0530

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using DatingApp.API.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Values> Values { get; set; }
+    }
+}
